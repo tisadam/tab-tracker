ea96cb8fbc03343169aaf0af93eca3a780bb4253
Adam Tiselius <adam.tiselius@meridion.se>
2018-02-27 14:27:56 +0100

    Doing uppdates to Register Frontend

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,25 +1,35 @@
 <template>
-<div>
-  <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-  <input
-    type="email"
-    name="email"
-    v-model="email"
-    placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-      <br>
-      <br>
-      <button
-        @click="register">
-        Register</button>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+          type="email"
+          name="email"
+          v-model="email"
+          placeholder="email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password" />
+            <br>
+            <div class="error" v-html="error" />
+            <br>
+            <v-btn
+              class="cyan"
+              @click="register">
+              Register</v-btn>
+        </div>
 
-</div>
+      </div>
+    </v-flex>
+  </v-layout>
 
 </template>
 
@@ -29,17 +39,22 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      console.log('register button was clicked with info:', this.email, this.password)
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      // console.log(response.data)
+      try {
+        console.log('register button was clicked with info:', this.email, this.password)
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        // console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -47,5 +62,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 
 </style>
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -1,6 +1,8 @@
 
 module.exports = {
   register (req, res, next) {
+  // Code for validating if Email is an email.
+  // Password is an okey Password
     next()
   }
 }
