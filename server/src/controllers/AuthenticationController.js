30dc065e4872592a09ec0eb28ffa862def46363c
Adam Tiselius <adam.tiselius@meridion.se>
2018-02-27 11:25:15 +0100

    Resettet sqlite after Joi install, ignore framework

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,8 +1,14 @@
+const {User} = require('../models')
 
 module.exports = {
-  register (req, res) {
-    res.send({
-      message: `Hello ${req.body.email} Your user was registered`
-    })
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(400).send({
+        error: 'Error, DB rules'
+      })
+    }
   }
 }
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  register (req, res, next) {
+    next()
+  }
+}
