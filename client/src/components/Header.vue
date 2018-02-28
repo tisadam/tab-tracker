a241b93d5e80807d690fb6b8bf31d08bf480ccfe
Adam Tiselius <adam.tiselius@meridion.se>
2018-02-28 16:08:13 +0100

    Front end work2

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,8 +1,26 @@
 <template>
-<v-toolbar fixed class="cyan">
+<v-toolbar fixed class="blue-grey lighten-4">
   <v-toolbar-title class="mr-4">
+<span class="home">
+    <router-link to="HelloWorld">
     HeaderTitle
+    </router-link>
+  </span>
   </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn flat dark>
+        Browse
+      </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <router-link to="register">
+          <v-btn flat dark>
+            Sign up
+          </v-btn>
+          </router-link>
+        </v-toolbar-items>
 </v-toolbar>
 </template>
 
@@ -14,5 +32,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.home {
+  cursor: pointer;
+}
 
+.home:hover {
+  color: #E9E;
+}
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,8 +8,8 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: '/HelloWorld',
+      name: 'root',
       component: HelloWorld
     },
     {
